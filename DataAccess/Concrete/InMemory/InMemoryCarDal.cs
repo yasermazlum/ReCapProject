192d801e0cfc7c22961e8857c9deb26f99ab0a1c
Manu <yasermazlum@gmail.com>
2021-03-05 19:25:53 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car{Id= 1, BrandId = 1, ColorId = 2, DailyPrice = 24000, ModelYear=2012, Description="Mercedes" },
+                new Car{Id= 2, BrandId = 2, ColorId = 1, DailyPrice = 11000, ModelYear=2012, Description="Opel" },
+                new Car{Id= 3, BrandId = 3, ColorId = 2, DailyPrice = 12000, ModelYear=2012, Description="BMW" },
+                new Car{Id= 4, BrandId = 4, ColorId = 1, DailyPrice = 12500, ModelYear=2012, Description="Ford" },
+                new Car{Id= 5, BrandId = 5, ColorId = 3, DailyPrice = 16000, ModelYear=2012, Description="Honda" },
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+            Console.WriteLine("Arac eklendi. Model Yili: " + car.ModelYear);
+        }
+
+        public void Delete(Car car)
+        {
+            //Car carDelete=null;
+            //foreach (var item in _cars)
+            //{
+            //    if (car.Id == item.Id)
+            //    {
+            //        carDelete = item;
+            //    }
+            //}
+
+
+            //Linq ile 
+            Car carDelete = _cars.SingleOrDefault(p => p.Id == car.Id);
+
+            _cars.Remove(carDelete);
+            Console.WriteLine("Arac silindi.");
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car carUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+            carUpdate.BrandId = car.BrandId;
+            carUpdate.ColorId = car.ColorId;
+            carUpdate.DailyPrice = car.DailyPrice;
+            carUpdate.Description = car.Description;
+            carUpdate.ModelYear = car.ModelYear;
+           
+
+            Console.WriteLine("Arac guncellendi. Model Yili: " + car.ModelYear);
+        }
+    }
+}
