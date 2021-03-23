c8a86a312745aeb92b35af8e5e36edbe3949eaba
Manu <yasermazlum@gmail.com>
2021-03-23 19:51:39 +0300

    API eklendi var Car icin singleton ayarlandi

---
+++ WebAPI/Controller/CarsController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public List<Car> Get()
+        {
+            var result = _carService.GetAll();
+            return result.Data;
+        }
+    }
+}
